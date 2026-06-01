2f401f41965959d9538ccc4783ce58f49ae5af18
12mrmack <12mrmack@gmail.com>
2026-06-01 23:15:06 +0530

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,5 @@
+@Library('Library_function') _
+
+def cfg = load 'deploy-config.groovy'
+
+opensearchDeploy(cfg)
---
+++ deploy-config.groovy
@@ -0,0 +1,7 @@
+return [
+    SLACK_CHANNEL_NAME  : "devops-alert",
+    ENVIRONMENT         : "prod",
+    CODE_BASE_PATH      : ".",
+    ACTION_MESSAGE      : "OpenSearch Deployment",
+    KEEP_APPROVAL_STAGE : true
+]
